6aca59b33ae15a612611f278ad639657e77d15cf
dylanclarke890 <dylanclarke890@gmail.com>
2024-01-02 23:36:07 +0000

    2D Renderer: transforms

--- Krystal/src/Krystal/Renderer/Renderer2D.cpp
+++ Krystal/src/Krystal/Renderer/Renderer2D.cpp
@@ -5,6 +5,8 @@
 #include "Krystal/Renderer/Shader.h"
 #include "Krystal/Renderer/VertexArray.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+
 namespace Krys
 {
   struct Renderer2DData
@@ -49,7 +51,6 @@ namespace Krys
   {
     s_Data->FlatColorShader->Bind();
     s_Data->FlatColorShader->SetMat4("u_ViewProjection", camera.GetViewProjectionMatrix());
-    s_Data->FlatColorShader->SetMat4("u_Transform", glm::mat4(1.0f));
   }
   
   void Renderer2D::EndScene()
@@ -66,6 +67,9 @@ namespace Krys
   {
     s_Data->FlatColorShader->Bind();
     s_Data->FlatColorShader->SetFloat4("u_Color", color);
+
+    glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) * glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
+    s_Data->FlatColorShader->SetMat4("u_Transform", transform);
 
     s_Data->QuadVertexArray->Bind();
     RenderCommand::DrawIndexed(s_Data->QuadVertexArray);
--- Sandbox/src/Layers/Sandbox2DLayer.cpp
+++ Sandbox/src/Layers/Sandbox2DLayer.cpp
@@ -48,7 +48,8 @@ void Sandbox2DLayer::OnUpdate(Krys::TimeStep ts)
 
 	Krys::Renderer2D::BeginScene(m_CameraController.GetCamera());
 	{
-		Krys::Renderer2D::DrawQuad({ 0.0f, 0.0f }, { 1.0f, 1.0f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+		Krys::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+		Krys::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
 	}
 	Krys::Renderer2D::EndScene();
 }
