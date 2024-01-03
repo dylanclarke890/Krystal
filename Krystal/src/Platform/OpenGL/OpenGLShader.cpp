e5c75e1e24f0b3ace9afd6ca92f9fbb378b0be22
dylanclarke890 <dylanclarke890@gmail.com>
2024-01-03 13:48:13 +0000

    Added file reading check

--- Krystal/src/Platform/OpenGL/OpenGLShader.cpp
+++ Krystal/src/Platform/OpenGL/OpenGLShader.cpp
@@ -128,9 +128,18 @@ namespace Krys
 		if (in)
 		{
 			in.seekg(0, std::ios::end);
-			result.resize(in.tellg());
-			in.seekg(0, std::ios::beg);
-			in.read(&result[0], result.size());
+			size_t size = in.tellg();
+			if (size != -1)
+			{
+				result.resize(size);
+				in.seekg(0, std::ios::beg);
+				in.read(&result[0], size);
+				in.close();
+			}
+			else
+			{
+				KRYS_CORE_ERROR("Could not read from file '{0}'", filePath);
+			}
 		}
 		else
 		{
