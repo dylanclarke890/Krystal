c210c2ebffdc49b2c887955f985251956942a44a
dylanclarke890 <dylanclarke890@gmail.com>
2024-01-04 05:04:41 +0000

    Application::Run can only be called privately and by the entry point of the engine (added main function as a friend of application).

--- Krystal/src/Krystal/Core/Application.h
+++ Krystal/src/Krystal/Core/Application.h
@@ -10,35 +10,37 @@
 
 #include "Krystal/ImGui/ImGuiLayer.h"
 
+int main(int argc, char** argv);
+
 namespace Krys
 {
   class Application
   {
   private:
     static Application* s_Instance;
-  public:
-    static inline Application& Get() { return *s_Instance; }
-
-  private:
     bool m_Running, m_Minimised;
     Scope<Window> m_Window;
     ImGuiLayer* m_ImGuiLayer;
     LayerStack m_LayerStack;
     float m_LastFrameTime;
 
-    bool OnWindowClose(WindowCloseEvent& e);
-    bool OnWindowResize(WindowResizeEvent& e);
+    friend int ::main(int argc, char** argv);
   public:
     Application();
     virtual ~Application();
 
-    void Run();
     void OnEvent(Event& e);
 
     void PushLayer(Layer* layer);
     void PushOverlay(Layer* overlay);
 
     inline Window& GetWindow() { return *m_Window; }
+  
+    static inline Application& Get() { return *s_Instance; }
+  private:
+    void Run();
+    bool OnWindowClose(WindowCloseEvent& e);
+    bool OnWindowResize(WindowResizeEvent& e);
   };
 
   // To be defined in CLIENT
