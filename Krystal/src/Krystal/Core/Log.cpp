4e5b594bf637093602e8b64307b1345382dfc490
dylanclarke890 <dylanclarke890@gmail.com>
2024-01-04 05:18:50 +0000

    output logging to file as well as console

--- Krystal/src/Krystal/Core/Log.cpp
+++ Krystal/src/Krystal/Core/Log.cpp
@@ -1,5 +1,7 @@
 #include "krys-pch.h"
+
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
 
 namespace Krys
 {
@@ -7,15 +9,22 @@ namespace Krys
   Ref<spdlog::logger> Log::s_ClientLogger;
   
   void Log::Init()
-  {
-    spdlog::set_pattern("%^[%T] %n: %v%$");
+	{
+		std::vector<spdlog::sink_ptr> logSinks;
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Krystal.log", true));
 
-    s_CoreLogger = spdlog::stdout_color_mt("KRYSTAL");
-    s_CoreLogger->set_level(spdlog::level::trace);
+		logSinks[0]->set_pattern("%^[%T] %n: %v%$");
+		logSinks[1]->set_pattern("[%T] [%l] %n: %v");
 
-    s_ClientLogger = spdlog::stdout_color_mt("APP");
-    s_ClientLogger->set_level(spdlog::level::trace);
+		s_CoreLogger = std::make_shared<spdlog::logger>("KRYSTAL", begin(logSinks), end(logSinks));
+		spdlog::register_logger(s_CoreLogger);
+		s_CoreLogger->set_level(spdlog::level::trace);
+		s_CoreLogger->flush_on(spdlog::level::trace);
 
-    KRYS_CORE_INFO("Start Up - Log Initialised.");
-  }
+		s_ClientLogger = std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
+		spdlog::register_logger(s_ClientLogger);
+		s_ClientLogger->set_level(spdlog::level::trace);
+		s_ClientLogger->flush_on(spdlog::level::trace);
+	}
 }
