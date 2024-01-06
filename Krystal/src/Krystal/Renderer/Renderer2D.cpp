d4b43518f8db67e550a3761a850de3eba8cc05a1
dylanclarke890 <dylanclarke890@gmail.com>
2024-01-06 05:37:10 +0000

    Fixed rendering an empty Renderer2D scene

--- Krystal/src/Krystal/Renderer/Renderer2D.cpp
+++ Krystal/src/Krystal/Renderer/Renderer2D.cpp
@@ -145,6 +145,9 @@ namespace Krys
   {
     KRYS_PROFILE_FUNCTION();
 
+    if (s_Data.QuadIndexCount == 0)
+      return; // Nothing to draw
+
     for (uint32_t i = 0; i < s_Data.TextureSlotIndex; i++)
       s_Data.TextureSlots[i]->Bind(i);
 
