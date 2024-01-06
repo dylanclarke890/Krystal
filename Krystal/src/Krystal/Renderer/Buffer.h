ceba7811edcd851b22e0cc302e2d1888665179f4
dylanclarke890 <dylanclarke890@gmail.com>
2024-01-06 04:09:32 +0000

    correctly handle passing mat3/mat4 as a vertex attribute in open gl

--- Krystal/src/Krystal/Renderer/Buffer.h
+++ Krystal/src/Krystal/Renderer/Buffer.h
@@ -11,17 +11,17 @@ namespace Krys
   {
     switch (type) 
     {
-      case ShaderDataType::Float:   return sizeof(float);
-      case ShaderDataType::Float2:  return sizeof(float) * 2;
-      case ShaderDataType::Float3:  return sizeof(float) * 3;
-      case ShaderDataType::Float4:  return sizeof(float) * 4;
-      case ShaderDataType::Mat3:    return sizeof(float) * 3 * 3;
-      case ShaderDataType::Mat4:    return sizeof(float) * 4 * 4;
+      case ShaderDataType::Bool:    return sizeof(bool);
       case ShaderDataType::Int:     return sizeof(int);
       case ShaderDataType::Int2:    return sizeof(int) * 2;
       case ShaderDataType::Int3:    return sizeof(int) * 3;
       case ShaderDataType::Int4:    return sizeof(int) * 4;
-      case ShaderDataType::Bool:    return sizeof(bool);
+      case ShaderDataType::Float:   return sizeof(float);
+      case ShaderDataType::Float2:  return sizeof(float) * 2;
+      case ShaderDataType::Float3:  return sizeof(float) * 3;
+      case ShaderDataType::Float4:  return sizeof(float) * 4;
+      case ShaderDataType::Mat3:    return 3; // 3 * float3
+      case ShaderDataType::Mat4:    return 4; // 4 * float4
     }
 
     KRYS_CORE_ASSERT(false, "Unknown ShaderDataType!");
