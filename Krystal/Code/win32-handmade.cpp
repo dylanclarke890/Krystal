1267fe2e1506e8d7fd82cd7d3c948c8546ca0a1c
dylanclarke890 <dylanclarke890@gmail.com>
2024-03-25 15:06:49 +0000

    basic project set up

---
+++ Krystal/Code/win32-handmade.cpp
@@ -0,0 +1,8 @@
+#include <windows.h>
+
+int WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+  MessageBoxA(0, "This is handmade hero.", "Handmade Hero", MB_OK | MB_ICONINFORMATION);
+
+  return 0;
+}
