238fe7eca5e3e059a18cf17f0828839a0db10902
dylanclarke890 <dylanclarke890@gmail.com>
2024-04-08 23:38:15 +0100

    tidy up entry

--- src/Platform/Windows/WindowsEntry.cpp
+++ src/Platform/Windows/WindowsEntry.cpp
@@ -1,27 +1,22 @@
 #include "WindowsWindow.h"
 
-static bool Win_ProcessMessages()
-{
-  MSG msg = {};
-  while (PeekMessageA(&msg, NULL, 0, 0, PM_REMOVE) > 0)
-  {
-    if (msg.message == WM_QUIT)
-      return false;
-
-    TranslateMessage(&msg);
-    DispatchMessageA(&msg);
-  }
-
-  return true;
-}
-
 int CALLBACK WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR cmdLine, int nCmdShow)
 {
   Krys::WindowsWindow *window = new Krys::WindowsWindow("Krystal Platform", instance, cmdLine, nCmdShow);
   window->Show(true);
 
-  while (Win_ProcessMessages())
+  MSG msg = {};
+  bool isRunning = true;
+  while (isRunning)
   {
+    while (PeekMessageA(&msg, NULL, 0, 0, PM_REMOVE) > 0)
+    {
+      if (msg.message == WM_QUIT)
+        isRunning = false;
+
+      TranslateMessage(&msg);
+      DispatchMessageA(&msg);
+    }
   }
 
   delete window;
