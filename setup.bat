da7ef0f8aa47127393ace7c0524b89e5bd7b2d37
dylanclarke890 <dylanclarke890@gmail.com>
2024-04-13 12:49:50 +0100

    organise bat scripts

--- setup.bat
+++ setup.bat
@@ -1,5 +1,5 @@
 @echo off
 REM Sets up the cmd shell environment so that `cl` and `devenv` is available to run.
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-set path=k:\misc;%path%
+set path=k:\build-scripts;%path%
 cd /d K:
