05d323c67e90d77225b8abf023f2ee7208b26b43
dylanclarke890 <dylanclarke890@gmail.com>
2024-04-17 23:03:56 +0100

    perspective drawing

--- src/Core/Application.cpp
+++ src/Core/Application.cpp
@@ -5,6 +5,9 @@
 #include <fstream>
 #include <sstream>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
 #include "Krystal.h"
 #include "Application.h"
 #include "Input/MouseButtons.h"
@@ -146,6 +149,24 @@ namespace Krys
 
     GLuint programID = LoadShaders("shader.vert", "shader.frag");
 
+    // Projection matrix: 45° Field of View, 4:3 ratio, display range: 0.1 unit <-> 100 units
+    Mat4 Projection = glm::perspective(glm::radians(45.0f), (float)780.0f / (float)1280.0f, 0.1f, 100.0f);
+
+    // Or, for an ortho camera:
+    // glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
+
+    // Camera matrix
+    Mat4 View = glm::lookAt(
+        Vec3(4, 3, 3), // Camera is at (4,3,3), in World Space
+        Vec3(0, 0, 0), // and looks at the origin
+        Vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
+    );
+
+    // Model matrix: an identity matrix (model will be at the origin)
+    Mat4 Model = Mat4(1.0f);
+    // Our ModelViewProjection: multiplication of our 3 matrices
+    Mat4 mvp = Projection * View * Model; // Remember, matrix multiplication is the other way around
+
     while (IsRunning)
     {
       int64 startCounter = Performance::GetTicks();
@@ -165,6 +186,15 @@ namespace Krys
             0,        // stride
             (void *)0 // array buffer offset
         );
+
+        // Get a handle for our "MVP" uniform
+        // Only during the initialisation
+        GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+
+        // Send our transformation to the currently bound shader, in the "MVP" uniform
+        // This is done in the main loop since each model will have a different MVP matrix (At least for the M part)
+        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvp[0][0]);
+
         // Draw the triangle !
         glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
         glDisableVertexAttribArray(0);
