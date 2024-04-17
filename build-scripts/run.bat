1c5b3c21e050f2208fbe733afca89764cf211a10
dylanclarke890 <dylanclarke890@gmail.com>
2024-04-17 22:42:37 +0100

    remove old code

--- build-scripts/run.bat
+++ build-scripts/run.bat
@@ -1,4 +1,4 @@
 @echo off
-REM Launches and runs the exe in debug mode using Visual Studio.
+REM Builds and runs the exe in debug mode using Visual Studio.
 call build.bat
 if %ERRORLEVEL% equ 0 devenv \build\WindowsEntry.exe /run \build\WindowsEntry.sln
