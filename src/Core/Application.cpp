a8cf9d7ce39990ececc9de214a3c984084a72029
dylanclarke890 <dylanclarke890@gmail.com>
2024-04-17 23:15:46 +0100

    color attributes

--- src/Core/Application.cpp
+++ src/Core/Application.cpp
@@ -125,35 +125,104 @@ namespace Krys
     glGenVertexArrays(1, &VertexArrayID);
     glBindVertexArray(VertexArrayID);
 
-    // An array of 3 vectors which represents 3 vertices
     static const GLfloat vertexBufferData[] = {
-        -1.0f,
-        -1.0f,
-        0.0f,
-        1.0f,
-        -1.0f,
-        0.0f,
-        0.0f,
-        1.0f,
-        0.0f,
-    };
+        -1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f, 1.0f,
+        -1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f, -1.0f,
+        -1.0f, -1.0f, -1.0f,
+        -1.0f, 1.0f, -1.0f,
+        1.0f, -1.0f, 1.0f,
+        -1.0f, -1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f,
+        1.0f, 1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f, -1.0f,
+        -1.0f, 1.0f, 1.0f,
+        -1.0f, 1.0f, -1.0f,
+        1.0f, -1.0f, 1.0f,
+        -1.0f, -1.0f, 1.0f,
+        -1.0f, -1.0f, -1.0f,
+        -1.0f, 1.0f, 1.0f,
+        -1.0f, -1.0f, 1.0f,
+        1.0f, -1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f, -1.0f, -1.0f,
+        1.0f, 1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f, -1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f, -1.0f,
+        -1.0f, 1.0f, -1.0f,
+        1.0f, 1.0f, 1.0f,
+        -1.0f, 1.0f, -1.0f,
+        -1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        -1.0f, 1.0f, 1.0f,
+        1.0f, -1.0f, 1.0f};
+
+    static const GLfloat colorBufferData[] = {
+        0.583f, 0.771f, 0.014f,
+        0.609f, 0.115f, 0.436f,
+        0.327f, 0.483f, 0.844f,
+        0.822f, 0.569f, 0.201f,
+        0.435f, 0.602f, 0.223f,
+        0.310f, 0.747f, 0.185f,
+        0.597f, 0.770f, 0.761f,
+        0.559f, 0.436f, 0.730f,
+        0.359f, 0.583f, 0.152f,
+        0.483f, 0.596f, 0.789f,
+        0.559f, 0.861f, 0.639f,
+        0.195f, 0.548f, 0.859f,
+        0.014f, 0.184f, 0.576f,
+        0.771f, 0.328f, 0.970f,
+        0.406f, 0.615f, 0.116f,
+        0.676f, 0.977f, 0.133f,
+        0.971f, 0.572f, 0.833f,
+        0.140f, 0.616f, 0.489f,
+        0.997f, 0.513f, 0.064f,
+        0.945f, 0.719f, 0.592f,
+        0.543f, 0.021f, 0.978f,
+        0.279f, 0.317f, 0.505f,
+        0.167f, 0.620f, 0.077f,
+        0.347f, 0.857f, 0.137f,
+        0.055f, 0.953f, 0.042f,
+        0.714f, 0.505f, 0.345f,
+        0.783f, 0.290f, 0.734f,
+        0.722f, 0.645f, 0.174f,
+        0.302f, 0.455f, 0.848f,
+        0.225f, 0.587f, 0.040f,
+        0.517f, 0.713f, 0.338f,
+        0.053f, 0.959f, 0.120f,
+        0.393f, 0.621f, 0.362f,
+        0.673f, 0.211f, 0.457f,
+        0.820f, 0.883f, 0.371f,
+        0.982f, 0.099f, 0.879f};
 
     // This will identify our vertex buffer
     GLuint vertexbuffer;
-    // Generate 1 buffer, put the resulting identifier in vertexbuffer
     glGenBuffers(1, &vertexbuffer);
-    // The following commands will talk about our 'vertexbuffer' buffer
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-    // Give our vertices to OpenGL.
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertexBufferData), vertexBufferData, GL_STATIC_DRAW);
 
-    GLuint programID = LoadShaders("shader.vert", "shader.frag");
+    GLuint colorbuffer;
+    glGenBuffers(1, &colorbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colorBufferData), colorBufferData, GL_STATIC_DRAW);
 
+    GLuint programID = LoadShaders("shader.vert", "shader.frag");
+    // Enable depth test
+    glEnable(GL_DEPTH_TEST);
+    // Accept fragment if it closer to the camera than the former one
+    glDepthFunc(GL_LESS);
+    
     // Projection matrix: 45° Field of View, 4:3 ratio, display range: 0.1 unit <-> 100 units
-    Mat4 Projection = glm::perspective(glm::radians(45.0f), (float)780.0f / (float)1280.0f, 0.1f, 100.0f);
+    // Mat4 Projection = glm::perspective(glm::radians(45.0f), (float)780.0f / (float)1280.0f, 0.1f, 100.0f);
 
     // Or, for an ortho camera:
-    // glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
+    Mat4 Projection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, 0.0f, 100.0f); // In world coordinates
 
     // Camera matrix
     Mat4 View = glm::lookAt(
@@ -187,6 +256,18 @@ namespace Krys
             (void *)0 // array buffer offset
         );
 
+        // 2nd attribute buffer : colors
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+        glVertexAttribPointer(
+            1,        // attribute. No particular reason for 1, but must match the layout in the shader.
+            3,        // size
+            GL_FLOAT, // type
+            GL_FALSE, // normalized?
+            0,        // stride
+            (void *)0 // array buffer offset
+        );
+
         // Get a handle for our "MVP" uniform
         // Only during the initialisation
         GLuint MatrixID = glGetUniformLocation(programID, "MVP");
@@ -196,7 +277,7 @@ namespace Krys
         glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvp[0][0]);
 
         // Draw the triangle !
-        glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+        glDrawArrays(GL_TRIANGLES, 0, 12 * 3); // 12*3 indices starting at 0 -> 12 triangles -> 6 squares
         glDisableVertexAttribArray(0);
       }
       input->EndFrame();
