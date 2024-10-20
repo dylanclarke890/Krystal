f72686293cc925fca801def83d83696ff25fcdf4
dylanclarke890 <dylanclarke890@gmail.com>
2024-10-20 18:44:21 +0100

    formatting

--- src/Core/Misc/PlatformDetection.h
+++ src/Core/Misc/PlatformDetection.h
@@ -1,39 +1,37 @@
 #pragma once
 
 #if defined(_WIN32) // Windows platform detection
-#if defined(_WIN64)
-#define KRYS_PLATFORM_WINDOWS
-#else
-#error "x86 Builds are not supported!"
-#endif
-
+  #if defined(_WIN64)
+    #define KRYS_PLATFORM_WINDOWS
+  #else
+    #error "x86 Builds are not supported!"
+  #endif
 #elif defined(__APPLE__) || defined(__MACH__) // Apple platform detection
-#include <TargetConditionals.h>
-// TARGET_OS_MAC exists on all platforms, not just on MacOS.
-// Use this order of checks to determine the specific platform.
-#if defined(TARGET_IPHONE_SIMULATOR)
-#error "IOS simulator is not supported!"
-#elif defined(TARGET_OS_IPHONE)
-#define KRYS_PLATFORM_IOS
-#error "IOS is not supported!"
-#elif defined(TARGET_OS_MAC)
-#define KRYS_PLATFORM_MACOS
-#error "MacOS is not supported!"
-#else
-#error "Unknown Apple platform!"
-#endif
-
+  #include <TargetConditionals.h>
+  // TARGET_OS_MAC exists on all platforms, not just on MacOS.
+  // Use this order of checks to determine the specific platform.
+  #if defined(TARGET_IPHONE_SIMULATOR)
+    #error "IOS simulator is not supported!"
+  #elif defined(TARGET_OS_IPHONE)
+    #define KRYS_PLATFORM_IOS
+    #error "IOS is not supported!"
+  #elif defined(TARGET_OS_MAC)
+    #define KRYS_PLATFORM_MACOS
+    #error "MacOS is not supported!"
+  #else
+    #error "Unknown Apple platform!"
+  #endif
 // Linux platform detection
 #elif defined(__linux__) // Apple platform detection
-// __linux__ exists on both android and linux.
-// Check for __ANDROID__ to determine the platform.
-#if defined(__ANDROID__)
-#define KRYS_PLATFORM_ANDROID
-#error "Android is not supported!"
+  // __linux__ exists on both android and linux.
+  // Check for __ANDROID__ to determine the platform.
+  #if defined(__ANDROID__)
+    #define KRYS_PLATFORM_ANDROID
+    #error "Android is not supported!"
+  #else
+    #define KRYS_PLATFORM_LINUX
+    #error "Linux is not supported!"
+  #endif
 #else
-#define KRYS_PLATFORM_LINUX
-#error "Linux is not supported!"
-#endif
-#else
-#error "Unknown platform!"
+  #error "Unknown platform!"
 #endif
--- src/Platform/OpenGL/Textures/Texture.h
+++ src/Platform/OpenGL/Textures/Texture.h
@@ -1,25 +1,26 @@
 #pragma once
 
+#include "Core.h"
 #include "Graphics/Textures/Texture.h"
 #include "OpenGL/GL.h"
 
 namespace Krys::OpenGL
 {
   using namespace Krys::Graphics;
-  
+
   static auto ToGLTextureMagnifyMode = [](TextureMagnifyMode mode)
   {
     switch (mode)
     {
-    case TextureMagnifyMode::Nearest:
-      return GL_NEAREST;
-    case TextureMagnifyMode::Linear:
-      return GL_LINEAR;
-    default:
-    {
-      KRYS_ASSERT(false, "Unknown TextureMagnifyMode", 0);
-      return 0;
-    }
+      case TextureMagnifyMode::Nearest:
+        return GL_NEAREST;
+      case TextureMagnifyMode::Linear:
+        return GL_LINEAR;
+      default:
+      {
+        KRYS_ASSERT(false, "Unknown TextureMagnifyMode", 0);
+        return 0;
+      }
     }
   };
 
@@ -27,23 +28,23 @@ namespace Krys::OpenGL
   {
     switch (mode)
     {
-    case TextureMinifyMode::Nearest:
-      return GL_NEAREST;
-    case TextureMinifyMode::NearestMipmapNearest:
-      return GL_NEAREST_MIPMAP_NEAREST;
-    case TextureMinifyMode::NearestMipmapLinear:
-      return GL_NEAREST_MIPMAP_LINEAR;
-    case TextureMinifyMode::Linear:
-      return GL_LINEAR;
-    case TextureMinifyMode::LinearMipmapNearest:
-      return GL_LINEAR_MIPMAP_NEAREST;
-    case TextureMinifyMode::LinearMipmapLinear:
-      return GL_LINEAR_MIPMAP_LINEAR;
-    default:
-    {
-      KRYS_ASSERT(false, "Unknown TextureMinifyMode", 0);
-      return 0;
-    }
+      case TextureMinifyMode::Nearest:
+        return GL_NEAREST;
+      case TextureMinifyMode::NearestMipmapNearest:
+        return GL_NEAREST_MIPMAP_NEAREST;
+      case TextureMinifyMode::NearestMipmapLinear:
+        return GL_NEAREST_MIPMAP_LINEAR;
+      case TextureMinifyMode::Linear:
+        return GL_LINEAR;
+      case TextureMinifyMode::LinearMipmapNearest:
+        return GL_LINEAR_MIPMAP_NEAREST;
+      case TextureMinifyMode::LinearMipmapLinear:
+        return GL_LINEAR_MIPMAP_LINEAR;
+      default:
+      {
+        KRYS_ASSERT(false, "Unknown TextureMinifyMode", 0);
+        return 0;
+      }
     }
   };
 
@@ -51,21 +52,21 @@ namespace Krys::OpenGL
   {
     switch (mode)
     {
-    case TextureWrapMode::Repeat:
-      return GL_REPEAT;
-    case TextureWrapMode::MirroredRepeat:
-      return GL_MIRRORED_REPEAT;
-    case TextureWrapMode::ClampToEdge:
-      return GL_CLAMP_TO_EDGE;
-    case TextureWrapMode::MirroredClampToEdge:
-      return GL_MIRROR_CLAMP_TO_EDGE;
-    case TextureWrapMode::ClampToBorder:
-      return GL_CLAMP_TO_BORDER;
-    default:
-    {
-      KRYS_ASSERT(false, "Unknown TextureWrapMode", 0);
-      return 0;
-    }
+      case TextureWrapMode::Repeat:
+        return GL_REPEAT;
+      case TextureWrapMode::MirroredRepeat:
+        return GL_MIRRORED_REPEAT;
+      case TextureWrapMode::ClampToEdge:
+        return GL_CLAMP_TO_EDGE;
+      case TextureWrapMode::MirroredClampToEdge:
+        return GL_MIRROR_CLAMP_TO_EDGE;
+      case TextureWrapMode::ClampToBorder:
+        return GL_CLAMP_TO_BORDER;
+      default:
+      {
+        KRYS_ASSERT(false, "Unknown TextureWrapMode", 0);
+        return 0;
+      }
     }
   };
 
@@ -73,20 +74,20 @@ namespace Krys::OpenGL
   {
     switch (internalFormat)
     {
-    case TextureInternalFormat::RGB:
-    case TextureInternalFormat::SRGB:
-      return GL_RGB;
-    case TextureInternalFormat::RGBA:
-    case TextureInternalFormat::SRGBA:
-      return GL_RGBA;
-    case TextureInternalFormat::Depth:
-      return GL_DEPTH_COMPONENT;
-    case TextureInternalFormat::Auto:
-    default:
-    {
-      KRYS_ASSERT(false, "Unsupported TextureInternalFormat", 0);
-      return 0;
-    }
+      case TextureInternalFormat::RGB:
+      case TextureInternalFormat::SRGB:
+        return GL_RGB;
+      case TextureInternalFormat::RGBA:
+      case TextureInternalFormat::SRGBA:
+        return GL_RGBA;
+      case TextureInternalFormat::Depth:
+        return GL_DEPTH_COMPONENT;
+      case TextureInternalFormat::Auto:
+      default:
+      {
+        KRYS_ASSERT(false, "Unsupported TextureInternalFormat", 0);
+        return 0;
+      }
     }
   };
 
@@ -94,24 +95,24 @@ namespace Krys::OpenGL
   {
     switch (internalFormat)
     {
-    case TextureInternalFormat::RGB:
-      return GL_RGB8;
-    case TextureInternalFormat::SRGB:
-      return GL_SRGB8;
-    case TextureInternalFormat::RGBA:
-      return GL_RGBA8;
-    case TextureInternalFormat::RGBA16F:
-      return GL_RGBA16F;
-    case TextureInternalFormat::SRGBA:
-      return GL_SRGB8_ALPHA8;
-    case TextureInternalFormat::Depth:
-      return GL_DEPTH_COMPONENT24;
-    case TextureInternalFormat::Auto:
-    default:
-    {
-      KRYS_ASSERT(false, "Unsupported TextureInternalFormat", 0);
-      return 0;
-    }
+      case TextureInternalFormat::RGB:
+        return GL_RGB8;
+      case TextureInternalFormat::SRGB:
+        return GL_SRGB8;
+      case TextureInternalFormat::RGBA:
+        return GL_RGBA8;
+      case TextureInternalFormat::RGBA16F:
+        return GL_RGBA16F;
+      case TextureInternalFormat::SRGBA:
+        return GL_SRGB8_ALPHA8;
+      case TextureInternalFormat::Depth:
+        return GL_DEPTH_COMPONENT24;
+      case TextureInternalFormat::Auto:
+      default:
+      {
+        KRYS_ASSERT(false, "Unsupported TextureInternalFormat", 0);
+        return 0;
+      }
     }
   };
 }
