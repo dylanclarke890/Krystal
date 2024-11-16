3619b31059c58872d4ff512c1a91db598c39ad53
dylanclarke890 <dylanclarke890@gmail.com>
2024-11-16 10:24:26 +0000

    renamed scalar/algorithms to limits to better suit it's purpose

--- include/Core/Maths/Normalization.hpp
+++ include/Core/Maths/Normalization.hpp
@@ -2,7 +2,7 @@
 
 #include "Base/Attributes.hpp"
 #include "Base/Types.hpp"
-#include "Core/Maths/Scalars/Algorithms.hpp"
+#include "Core/Maths/Scalars/Limits.hpp"
 
 #include <algorithm>
 #include <cmath>
