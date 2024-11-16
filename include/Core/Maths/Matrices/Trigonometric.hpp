6cb29eed4a299c6207ef2f4ff3139ae5846df2c8
dylanclarke890 <dylanclarke890@gmail.com>
2024-11-16 09:06:36 +0000

    add trigonometric functions that operate on matrices

---
+++ include/Core/Maths/Matrices/Trigonometric.hpp
@@ -0,0 +1,135 @@
+#pragma once
+
+#include "Base/Attributes.hpp"
+#include "Base/Concepts.hpp"
+#include "Base/Types.hpp"
+#include "Core/Maths/Matrices/Algorithms.hpp"
+#include "Core/Maths/Matrices/Base.hpp"
+#include "Core/Maths/Scalars/Trigonometric.hpp"
+
+namespace Krys::MTL
+{
+  /// @brief Converts each component in `v` to radians.
+  /// @tparam TComponent The underlying arithmetic type of the input matrix.
+  /// @tparam CL The column length of the input matrix.
+  /// @tparam RL The row length of the input matrix.
+  /// @param v The input matrix.
+  /// @returns The converted matrix.
+  template <IsArithmeticT TComponent, vec_length_t CL, vec_length_t RL>
+  constexpr NO_DISCARD matrix_t<TComponent, CL, RL> Radians(matrix_t<TComponent, CL, RL> v) noexcept
+  {
+    using T = TComponent;
+    return MTL::Map<T, T, CL, RL>(v, [](T val) -> T { return MTL::Radians<T>(val); });
+  }
+
+  /// @brief Converts each component in `v` to degrees.
+  /// @tparam TComponent The underlying arithmetic type of the input matrix.
+  /// @tparam CL The column length of the input matrix.
+  /// @tparam RL The row length of the input matrix.
+  /// @param v The input matrix.
+  /// @returns The converted matrix.
+  template <IsArithmeticT TComponent, vec_length_t CL, vec_length_t RL>
+  constexpr NO_DISCARD matrix_t<TComponent, CL, RL> Degrees(matrix_t<TComponent, CL, RL> v) noexcept
+  {
+    using T = TComponent;
+    return MTL::Map<T, T, CL, RL>(v, [](T val) -> T { return MTL::Degrees<T>(val); });
+  }
+
+  /// @brief Computes sine for each component in `v`.
+  /// Each component should be an angle represented in radians.
+  /// @tparam TComponent The underlying arithmetic type of the input matrix.
+  /// @tparam CL The column length of the input matrix.
+  /// @tparam RL The row length of the input matrix.
+  /// @param v The input matrix.
+  /// @returns The converted matrix.
+  template <IsArithmeticT TComponent, vec_length_t CL, vec_length_t RL>
+  constexpr NO_DISCARD matrix_t<TComponent, CL, RL> Sin(matrix_t<TComponent, CL, RL> v) noexcept
+  {
+    using T = TComponent;
+    return MTL::Map<T, T, CL, RL>(v, [](T val) -> T { return MTL::Sin<T>(val); });
+  }
+
+  /// @brief Computes cosine for each component in `v`.
+  /// Each component should be an angle represented in radians.
+  /// @tparam TComponent The underlying arithmetic type of the input matrix.
+  /// @tparam CL The column length of the input matrix.
+  /// @tparam RL The row length of the input matrix.
+  /// @param v The input matrix.
+  /// @returns The converted matrix.
+  template <IsArithmeticT TComponent, vec_length_t CL, vec_length_t RL>
+  constexpr NO_DISCARD matrix_t<TComponent, CL, RL> Cos(matrix_t<TComponent, CL, RL> v) noexcept
+  {
+    using T = TComponent;
+    return MTL::Map<T, T, CL, RL>(v, [](T val) -> T { return MTL::Cos<T>(val); });
+  }
+
+  /// @brief Computes tangent for each component in `v`.
+  /// Each component should be an angle represented in radians.
+  /// @tparam TComponent The underlying arithmetic type of the input matrix.
+  /// @tparam CL The column length of the input matrix.
+  /// @tparam RL The row length of the input matrix.
+  /// @param v The input matrix.
+  /// @returns The converted matrix.
+  template <IsArithmeticT TComponent, vec_length_t CL, vec_length_t RL>
+  constexpr NO_DISCARD matrix_t<TComponent, CL, RL> Tan(matrix_t<TComponent, CL, RL> v) noexcept
+  {
+    using T = TComponent;
+    return MTL::Map<T, T, CL, RL>(v, [](T val) -> T { return MTL::Tan<T>(val); });
+  }
+
+  /// @brief Computes arc sine for each component in `v`.
+  /// Each component should be an angle represented in radians.
+  /// @tparam TComponent The underlying arithmetic type of the input matrix.
+  /// @tparam CL The column length of the input matrix.
+  /// @tparam RL The row length of the input matrix.
+  /// @param v The input matrix.
+  /// @returns The converted matrix.
+  template <IsArithmeticT TComponent, vec_length_t CL, vec_length_t RL>
+  constexpr NO_DISCARD matrix_t<TComponent, CL, RL> Asin(matrix_t<TComponent, CL, RL> v) noexcept
+  {
+    using T = TComponent;
+    return MTL::Map<T, T, CL, RL>(v, [](T val) -> T { return MTL::Asin<T>(val); });
+  }
+
+  /// @brief Computes arc cosine for each component in `v`.
+  /// Each component should be an angle represented in radians.
+  /// @tparam TComponent The underlying arithmetic type of the input matrix.
+  /// @tparam CL The column length of the input matrix.
+  /// @tparam RL The row length of the input matrix.
+  /// @param v The input matrix.
+  /// @returns The converted matrix.
+  template <IsArithmeticT TComponent, vec_length_t CL, vec_length_t RL>
+  constexpr NO_DISCARD matrix_t<TComponent, CL, RL> Acos(matrix_t<TComponent, CL, RL> v) noexcept
+  {
+    using T = TComponent;
+    return MTL::Map<T, T, CL, RL>(v, [](T val) -> T { return MTL::Acos<T>(val); });
+  }
+
+  /// @brief Computes arc tangent for each component in `v`.
+  /// Each component should be an angle represented in radians.
+  /// @tparam TComponent The underlying arithmetic type of the input matrix.
+  /// @tparam CL The column length of the input matrix.
+  /// @tparam RL The row length of the input matrix.
+  /// @param v The input matrix.
+  /// @returns The converted matrix.
+  template <IsArithmeticT TComponent, vec_length_t CL, vec_length_t RL>
+  constexpr NO_DISCARD matrix_t<TComponent, CL, RL> Atan(matrix_t<TComponent, CL, RL> v) noexcept
+  {
+    using T = TComponent;
+    return MTL::Map<T, T, CL, RL>(v, [](T val) -> T { return MTL::Atan<T>(val); });
+  }
+
+  /// @brief Computes arc tangent for each component in `v`, using signs to determine quadrants.
+  /// Each component should be an angle represented in radians.
+  /// @tparam TComponent The underlying arithmetic type of the input matrix.
+  /// @tparam CL The column length of the input matrix.
+  /// @tparam RL The row length of the input matrix.
+  /// @param v The input matrix.
+  /// @returns The converted matrix.
+  template <IsArithmeticT TComponent, vec_length_t CL, vec_length_t RL>
+  constexpr NO_DISCARD matrix_t<TComponent, CL, RL> Atan2(matrix_t<TComponent, CL, RL> v) noexcept
+  {
+    using T = TComponent;
+    return MTL::Map<T, T, CL, RL>(v, [](T val) -> T { return MTL::Atan2<T>(val); });
+  }
+}
