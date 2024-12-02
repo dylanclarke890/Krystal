6bdc578b97d333276fcbf2c27246af6583713a1b
dylanclarke890 <dylanclarke890@gmail.com>
2024-12-02 17:52:15 +0000

    tidy up transpose

--- include/MTL/Matrices/Ext/Transpose.hpp
+++ include/MTL/Matrices/Ext/Transpose.hpp
@@ -14,18 +14,23 @@ namespace Krys::MTL
   NO_DISCARD constexpr auto
     Transpose(const matrix_t<TComponent, CL, RL> &m) noexcept -> matrix_t<TComponent, CL, RL>::transpose_t
   {
+    static_assert(CL <= 4 && RL <= 4, "Unsupported number of cols/rows.");
     using transpose_t = matrix_t<TComponent, CL, RL>::transpose_t;
     using col_t = transpose_t::column_t;
 
     if constexpr (CL == 2 && RL == 2)
+    {
       return transpose_t(col_t {m[0].x, m[1].x}, col_t {m[0].y, m[1].y});
+    }
     else if constexpr (CL == 3 && RL == 3)
+    {
       return transpose_t(col_t {m[0].x, m[1].x, m[2].x}, col_t {m[0].y, m[1].y, m[2].y},
                          col_t {m[0].z, m[1].z, m[2].z});
+    }
     else if constexpr (CL == 4 && RL == 4)
+    {
       return transpose_t(col_t {m[0].x, m[1].x, m[2].x, m[3].x}, col_t {m[0].y, m[1].y, m[2].y, m[3].y},
                          col_t {m[0].z, m[1].z, m[2].z, m[3].z}, col_t {m[0].w, m[1].w, m[2].w, m[3].w});
-    else
-      static_assert(false, "Unsupported number of cols/rows.");
+    }
   }
 }
