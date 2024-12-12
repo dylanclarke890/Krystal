30d40268373ce89ff2d5fe7a60432e307ad4b5c9
dylanclarke890 <dylanclarke890@gmail.com>
2024-12-12 08:04:42 +0000

    more encoding methods for morton codes along with a few more for fun

--- include/MTL/MortonCodes.hpp
+++ include/MTL/MortonCodes.hpp
@@ -4,6 +4,11 @@
 #include "Base/Concepts.hpp"
 #include "Base/Macros.hpp"
 #include "Base/Types.hpp"
+#include "MTL/Vectors/Vec2.hpp"
+#include "MTL/Vectors/Vec3.hpp"
+#include "MTL/Vectors/Vec4.hpp"
+
+#include <bit>
 
 namespace Krys::MTL::Morton
 {
@@ -26,7 +31,7 @@ namespace Krys::MTL::Morton
     return a | static_cast<uint16>(b << 1);
   }
 
-  NO_DISCARD constexpr uint16 Encode(uint8 x, uint8 y, uint8 z) noexcept
+  NO_DISCARD constexpr uint32 Encode(uint8 x, uint8 y, uint8 z) noexcept
   {
     uint32 a(x);
     uint32 b(y);
@@ -76,7 +81,94 @@ namespace Krys::MTL::Morton
     return a | (b << 1) | (c << 2) | (d << 3);
   }
 
+  NO_DISCARD constexpr uint16 Encode(const vec2_t<uint8> &v) noexcept
+  {
+    return Encode(v.x, v.y);
+  }
+
+  NO_DISCARD constexpr uint32 Encode(const vec3_t<uint8> &v) noexcept
+  {
+    return Encode(v.x, v.y, v.z);
+  }
+
+  NO_DISCARD constexpr uint32 Encode(const vec4_t<uint8> &v) noexcept
+  {
+    return Encode(v.x, v.y, v.z, v.w);
+  }
+
+  NO_DISCARD constexpr vec2_t<uint8> Decode(uint16 x)
+  {
+    uint16 a(x);
+    uint16 b(x >> 1);
+
+    a = a & static_cast<uint16>(0x5555);
+    b = b & static_cast<uint16>(0x5555);
+
+    a = ((a >> 1) | a) & static_cast<uint16>(0x3333);
+    b = ((b >> 1) | b) & static_cast<uint16>(0x3333);
+
+    a = ((a >> 2) | a) & static_cast<uint16>(0x0F0F);
+    b = ((b >> 2) | b) & static_cast<uint16>(0x0F0F);
+
+    a = ((a >> 4) | a) & static_cast<uint16>(0x00FF);
+    b = ((b >> 4) | b) & static_cast<uint16>(0x00FF);
+
+    a = ((a >> 8) | a) & static_cast<uint16>(0xFFFF);
+    b = ((b >> 8) | b) & static_cast<uint16>(0xFFFF);
+
+    return vec2_t<uint8>(a, b);
+  }
+
 #pragma endregion uint8
+
+#pragma region int8
+
+  NO_DISCARD constexpr int16 Encode(int8 x, int8 y) noexcept
+  {
+    uint8 a = std::bit_cast<uint8>(x);
+    uint8 b = std::bit_cast<uint8>(y);
+
+    uint16 result = Encode(a, b);
+    return std::bit_cast<int16>(result);
+  }
+
+  NO_DISCARD constexpr int32 Encode(int8 x, int8 y, int8 z)
+  {
+    uint8 a = std::bit_cast<uint8>(x);
+    uint8 b = std::bit_cast<uint8>(y);
+    uint8 c = std::bit_cast<uint8>(z);
+
+    uint32 result = Encode(a, b, c);
+    return std::bit_cast<int32>(result);
+  }
+
+  NO_DISCARD constexpr int32 Encode(int8 x, int8 y, int8 z, int8 w)
+  {
+    uint8 a = std::bit_cast<uint8>(x);
+    uint8 b = std::bit_cast<uint8>(y);
+    uint8 c = std::bit_cast<uint8>(z);
+    uint8 d = std::bit_cast<uint8>(w);
+
+    uint32 result = Encode(a, b, c, d);
+    return std::bit_cast<int32>(result);
+  }
+
+  NO_DISCARD constexpr int16 Encode(const vec2_t<int8> &v) noexcept
+  {
+    return Encode(v.x, v.y);
+  }
+
+  NO_DISCARD constexpr int32 Encode(const vec3_t<int8> &v) noexcept
+  {
+    return Encode(v.x, v.y, v.z);
+  }
+
+  NO_DISCARD constexpr int32 Encode(const vec4_t<int8> &v) noexcept
+  {
+    return Encode(v.x, v.y, v.z, v.w);
+  }
+
+#pragma endregion int8
 
 #pragma region uint16
 
@@ -100,7 +192,7 @@ namespace Krys::MTL::Morton
     return a | (b << 1);
   }
 
-  NO_DISCARD constexpr uint32 Encode(uint16 x, uint16 y, uint16 z) noexcept
+  NO_DISCARD constexpr uint64 Encode(uint16 x, uint16 y, uint16 z) noexcept
   {
     uint64 a(x);
     uint64 b(y);
@@ -159,7 +251,94 @@ namespace Krys::MTL::Morton
     return a | (b << 1) | (c << 2) | (d << 3);
   }
 
+  NO_DISCARD constexpr uint32 Encode(const vec2_t<uint16> &v) noexcept
+  {
+    return Encode(v.x, v.y);
+  }
+
+  NO_DISCARD constexpr uint64 Encode(const vec3_t<uint16> &v) noexcept
+  {
+    return Encode(v.x, v.y, v.z);
+  }
+
+  NO_DISCARD constexpr uint64 Encode(const vec4_t<uint16> &v) noexcept
+  {
+    return Encode(v.x, v.y, v.z, v.w);
+  }
+
+  NO_DISCARD constexpr vec2_t<uint16> Decode(uint32 x)
+  {
+    uint32 a(x);
+    uint32 b(x >> 1);
+
+    a = a & static_cast<uint32>(0x55'55'55'55);
+    b = b & static_cast<uint32>(0x55'55'55'55);
+
+    a = ((a >> 1) | a) & static_cast<uint32>(0x33'33'33'33);
+    b = ((b >> 1) | b) & static_cast<uint32>(0x33'33'33'33);
+
+    a = ((a >> 2) | a) & static_cast<uint32>(0x0F'0F'0F'0F);
+    b = ((b >> 2) | b) & static_cast<uint32>(0x0F'0F'0F'0F);
+
+    a = ((a >> 4) | a) & static_cast<uint32>(0x00'FF'00'FF);
+    b = ((b >> 4) | b) & static_cast<uint32>(0x00'FF'00'FF);
+
+    a = ((a >> 8) | a) & static_cast<uint32>(0x00'00'FF'FF);
+    b = ((b >> 8) | b) & static_cast<uint32>(0x00'00'FF'FF);
+
+    return vec2_t<uint16>(a, b);
+  }
+
 #pragma endregion uint16
+
+#pragma region int16
+
+  NO_DISCARD constexpr int32 Encode(int16 x, int16 y) noexcept
+  {
+    uint16 a = std::bit_cast<uint16>(x);
+    uint16 b = std::bit_cast<uint16>(y);
+
+    uint32 result = Encode(a, b);
+    return std::bit_cast<int32>(result);
+  }
+
+  NO_DISCARD constexpr int64 Encode(int16 x, int16 y, int16 z) noexcept
+  {
+    uint16 a = std::bit_cast<uint16>(x);
+    uint16 b = std::bit_cast<uint16>(y);
+    uint16 c = std::bit_cast<uint16>(z);
+
+    uint64 result = Encode(a, b, c);
+    return std::bit_cast<int64>(result);
+  }
+
+  NO_DISCARD constexpr int64 Encode(int16 x, int16 y, int16 z, int16 w) noexcept
+  {
+    uint16 a = std::bit_cast<uint16>(x);
+    uint16 b = std::bit_cast<uint16>(y);
+    uint16 c = std::bit_cast<uint16>(z);
+    uint16 d = std::bit_cast<uint16>(w);
+
+    uint64 result = Encode(a, b, c, d);
+    return std::bit_cast<int64>(result);
+  }
+
+  NO_DISCARD constexpr int32 Encode(const vec2_t<int16> &v) noexcept
+  {
+    return Encode(v.x, v.y);
+  }
+
+  NO_DISCARD constexpr int64 Encode(const vec3_t<int16> &v) noexcept
+  {
+    return Encode(v.x, v.y, v.z);
+  }
+
+  NO_DISCARD constexpr int64 Encode(const vec4_t<int16> &v) noexcept
+  {
+    return Encode(v.x, v.y, v.z, v.w);
+  }
+
+#pragma endregion int16
 
 #pragma region uint32
 
@@ -215,6 +394,74 @@ namespace Krys::MTL::Morton
     return a | (b << 1) | (c << 2);
   }
 
+  NO_DISCARD constexpr uint64 Encode(const vec2_t<uint32> &v) noexcept
+  {
+    return Encode(v.x, v.y);
+  }
+
+  NO_DISCARD constexpr uint64 Encode(const vec3_t<uint32> &v) noexcept
+  {
+    return Encode(v.x, v.y, v.z);
+  }
+
+  NO_DISCARD constexpr vec2_t<uint32> Decode(uint64 x)
+  {
+    uint64 a(x);
+    uint64 b(x >> 1);
+
+    a = a & static_cast<uint64>(0x55'55'55'55'55'55'55'55ull);
+    b = b & static_cast<uint64>(0x55'55'55'55'55'55'55'55ull);
+
+    a = ((a >> 1) | a) & static_cast<uint64>(0x33'33'33'33'33'33'33'33ull);
+    b = ((b >> 1) | b) & static_cast<uint64>(0x33'33'33'33'33'33'33'33ull);
+
+    a = ((a >> 2) | a) & static_cast<uint64>(0x0F'0F'0F'0F'0F'0F'0F'0Full);
+    b = ((b >> 2) | b) & static_cast<uint64>(0x0F'0F'0F'0F'0F'0F'0F'0Full);
+
+    a = ((a >> 4) | a) & static_cast<uint64>(0x00'FF'00'FF'00'FF'00'FFull);
+    b = ((b >> 4) | b) & static_cast<uint64>(0x00'FF'00'FF'00'FF'00'FFull);
+
+    a = ((a >> 8) | a) & static_cast<uint64>(0x00'00'FF'FF'00'00'FF'FFull);
+    b = ((b >> 8) | b) & static_cast<uint64>(0x00'00'FF'FF'00'00'FF'FFull);
+
+    a = ((a >> 16) | a) & static_cast<uint64>(0x00'00'00'00'FF'FF'FF'FFull);
+    b = ((b >> 16) | b) & static_cast<uint64>(0x00'00'00'00'FF'FF'FF'FFull);
+
+    return vec2_t<uint32>(a, b);
+  }
+
 #pragma endregion uint32
 
+#pragma region int32
+
+  NO_DISCARD constexpr int64 Encode(int32 x, int32 y) noexcept
+  {
+    uint32 a = std::bit_cast<uint32>(x);
+    uint32 b = std::bit_cast<uint32>(y);
+
+    uint64 result = Encode(a, b);
+    return std::bit_cast<int64>(result);
+  }
+
+  NO_DISCARD constexpr int64 Encode(int32 x, int32 y, int32 z) noexcept
+  {
+    uint32 a = std::bit_cast<uint32>(x);
+    uint32 b = std::bit_cast<uint32>(y);
+    uint32 c = std::bit_cast<uint32>(z);
+
+    uint64 result = Encode(a, b, c);
+    return std::bit_cast<int64>(result);
+  }
+
+  NO_DISCARD constexpr int64 Encode(const vec2_t<int32> &v) noexcept
+  {
+    return Encode(v.x, v.y);
+  }
+
+  NO_DISCARD constexpr int64 Encode(const vec3_t<int32> &v) noexcept
+  {
+    return Encode(v.x, v.y, v.z);
+  }
+
+#pragma endregion int32
 }
--- tests/MTL/Common/MortonCodes.cpp
+++ tests/MTL/Common/MortonCodes.cpp
@@ -1,9 +1,14 @@
 #include "MTL/MortonCodes.hpp"
 #include "Core/Debug/Expect.hpp"
+#include "MTL/Vectors/Vec2.hpp"
+#include "MTL/Vectors/Vec3.hpp"
+#include "MTL/Vectors/Vec4.hpp"
 
 namespace Krys::Tests
 {
   using namespace Krys::MTL::Morton;
+
+#pragma region uint8
 
   static void Test_MortonCodes_Encode_2_uint8()
   {
@@ -16,6 +21,18 @@ namespace Krys::Tests
     KRYS_EXPECT_EQUAL("MortonCodes Encode (uint8, uint8)", result, expected);
   }
 
+  static void Test_MortonCodes_Encode_Vec2_uint8()
+  {
+    constexpr uint8 x = 0b11'111'111;
+    constexpr uint8 y = 0b00'000'000;
+    constexpr vec2_t<uint8> v(x, y);
+
+    constexpr uint16 expected = 0b0'101'010'101'010'101;
+    constexpr uint16 result = Encode(v);
+
+    KRYS_EXPECT_EQUAL("MortonCodes Encode Vec2(uint8, uint8)", result, expected);
+  }
+
   static void Test_MortonCodes_Encode_3_uint8()
   {
     constexpr uint8 x = 5;
@@ -26,6 +43,19 @@ namespace Krys::Tests
     constexpr uint16 result = Encode(x, y, z);
 
     KRYS_EXPECT_EQUAL("MortonCodes Encode (uint8, uint8, uint8)", result, expected);
+  }
+
+  static void Test_MortonCodes_Encode_Vec3_uint8()
+  {
+    constexpr uint8 x = 5;
+    constexpr uint8 y = 9;
+    constexpr uint8 z = 1;
+    constexpr vec3_t<uint8> v(x, y, z);
+
+    constexpr uint16 expected = 0b010'001'000'111;
+    constexpr uint16 result = Encode(v);
+
+    KRYS_EXPECT_EQUAL("MortonCodes Encode Vec3(uint8, uint8, uint8)", result, expected);
   }
 
   static void Test_MortonCodes_Encode_4_uint8()
@@ -41,6 +71,113 @@ namespace Krys::Tests
     KRYS_EXPECT_EQUAL("MortonCodes Encode (uint8, uint8, uint8, uint8)", result, expected);
   }
 
+  static void Test_MortonCodes_Encode_Vec4_uint8()
+  {
+    constexpr uint8 x = 0b11'111'111;
+    constexpr uint8 y = 0b11'111'111;
+    constexpr uint8 z = 0b00'000'000;
+    constexpr uint8 w = 0b00'000'000;
+    constexpr vec4_t<uint8> v(x, y, z, w);
+
+    constexpr uint32 expected = 0b00'110'011'001'100'110'011'001'100'110'011;
+    constexpr uint32 result = Encode(v);
+
+    KRYS_EXPECT_EQUAL("MortonCodes Encode Vec4(uint8, uint8, uint8, uint8)", result, expected);
+  }
+
+  static void Test_MortonCodes_Decode_uint16()
+  {
+    constexpr uint16 v = 0b0'101'010'101'010'101;
+    constexpr vec2_t<uint8> expected(0b11'111'111, 0b00'000'000);
+
+    constexpr auto result = Decode(v);
+
+    KRYS_EXPECT_EQUAL("MortonCodes Decode uint16", result, expected);
+  }
+
+#pragma endregion uint8
+
+#pragma region int8
+
+  static void Test_MortonCodes_Encode_2_int8()
+  {
+    constexpr int8 x = 0b11'111'111;
+    constexpr int8 y = 0b00'000'000;
+
+    constexpr int16 expected = 0b0'101'010'101'010'101;
+    constexpr int16 result = Encode(x, y);
+
+    KRYS_EXPECT_EQUAL("MortonCodes Encode (int8, int8)", result, expected);
+  }
+
+  static void Test_MortonCodes_Encode_Vec2_int8()
+  {
+    constexpr int8 x = 0b11'111'111;
+    constexpr int8 y = 0b00'000'000;
+    constexpr vec2_t<int8> v(x, y);
+
+    constexpr int16 expected = 0b0'101'010'101'010'101;
+    constexpr int16 result = Encode(v);
+
+    KRYS_EXPECT_EQUAL("MortonCodes Encode Vec2(int8, int8)", result, expected);
+  }
+
+  static void Test_MortonCodes_Encode_3_int8()
+  {
+    constexpr int8 x = 5;
+    constexpr int8 y = 9;
+    constexpr int8 z = 1;
+
+    constexpr int32 expected = 0b010'001'000'111;
+    constexpr int32 result = Encode(x, y, z);
+
+    KRYS_EXPECT_EQUAL("MortonCodes Encode (int8, int8, int8)", result, expected);
+  }
+
+  static void Test_MortonCodes_Encode_Vec3_int8()
+  {
+    constexpr int8 x = 5;
+    constexpr int8 y = 9;
+    constexpr int8 z = 1;
+    constexpr vec3_t<int8> v(x, y, z);
+
+    constexpr int16 expected = 0b010'001'000'111;
+    constexpr int16 result = Encode(v);
+
+    KRYS_EXPECT_EQUAL("MortonCodes Encode Vec3(int8, int8, int8)", result, expected);
+  }
+
+  static void Test_MortonCodes_Encode_4_int8()
+  {
+    constexpr int8 x = 0b11'111'111;
+    constexpr int8 y = 0b11'111'111;
+    constexpr int8 z = 0b00'000'000;
+    constexpr int8 w = 0b00'000'000;
+
+    constexpr int32 expected = 0b00'110'011'001'100'110'011'001'100'110'011;
+    constexpr int32 result = Encode(x, y, z, w);
+
+    KRYS_EXPECT_EQUAL("MortonCodes Encode (int8, int8, int8, int8)", result, expected);
+  }
+
+  static void Test_MortonCodes_Encode_Vec4_int8()
+  {
+    constexpr int8 x = 0b11'111'111;
+    constexpr int8 y = 0b11'111'111;
+    constexpr int8 z = 0b00'000'000;
+    constexpr int8 w = 0b00'000'000;
+    constexpr vec4_t<int8> v(x, y, z, w);
+
+    constexpr int32 expected = 0b00'110'011'001'100'110'011'001'100'110'011;
+    constexpr int32 result = Encode(v);
+
+    KRYS_EXPECT_EQUAL("MortonCodes Encode Vec4(int8, int8, int8, int8)", result, expected);
+  }
+
+#pragma endregion int8
+
+#pragma region uint16
+
   static void Test_MortonCodes_Encode_2_uint16()
   {
     constexpr uint16 x = 0b1'111'111'111'111'111;
@@ -50,6 +187,19 @@ namespace Krys::Tests
     constexpr uint32 result = Encode(x, y);
 
     KRYS_EXPECT_EQUAL("MortonCodes Encode (uint16, uint16)", result, expected);
+  }
+
+  static void Test_MortonCodes_Encode_Vec2_uint16()
+  {
+    constexpr uint16 x = 0b1'111'111'111'111'111;
+    constexpr uint16 y = 0b0'000'000'000'000'000;
+
+    constexpr vec2_t<uint16> v(x, y);
+
+    constexpr uint32 expected = 0b01'010'101'010'101'010'101'010'101'010'101;
+    constexpr uint32 result = Encode(v);
+
+    KRYS_EXPECT_EQUAL("MortonCodes Encode Vec3(uint16, uint16)", result, expected);
   }
 
   static void Test_MortonCodes_Encode_3_uint16()
@@ -64,6 +214,19 @@ namespace Krys::Tests
     KRYS_EXPECT_EQUAL("MortonCodes Encode (uint16, uint16, uint16)", result, expected);
   }
 
+  static void Test_MortonCodes_Encode_Vec3_uint16()
+  {
+    constexpr uint16 x = 5;
+    constexpr uint16 y = 9;
+    constexpr uint16 z = 1;
+    constexpr vec3_t<uint16> v(x, y, z);
+
+    constexpr uint32 expected = 1'095;
+    constexpr uint32 result = Encode(v);
+
+    KRYS_EXPECT_EQUAL("MortonCodes Encode Vec3(uint16, uint16, uint16)", result, expected);
+  }
+
   static void Test_MortonCodes_Encode_4_uint16()
   {
     constexpr uint16 x = 0b1'111'111'111'111'111;
@@ -75,8 +238,122 @@ namespace Krys::Tests
       0b0'011'001'100'110'011'001'100'110'011'001'100'110'011'001'100'110'011'001'100'110'011;
     constexpr uint64 result = Encode(x, y, z, w);
 
-    KRYS_EXPECT_EQUAL("MortonCodes Encode (uint8, uint8, uint8, uint8)", result, expected);
+    KRYS_EXPECT_EQUAL("MortonCodes Encode (uint16, uint16, uint16, uint16)", result, expected);
   }
+
+  static void Test_MortonCodes_Encode_Vec4_uint16()
+  {
+    constexpr uint16 x = 0b1'111'111'111'111'111;
+    constexpr uint16 y = 0b1'111'111'111'111'111;
+    constexpr uint16 z = 0b0'000'000'000'000'000;
+    constexpr uint16 w = 0b0'000'000'000'000'000;
+    constexpr vec4_t<uint16> v(x, y, z, w);
+
+    constexpr uint64 expected =
+      0b0'011'001'100'110'011'001'100'110'011'001'100'110'011'001'100'110'011'001'100'110'011;
+    constexpr uint64 result = Encode(v);
+
+    KRYS_EXPECT_EQUAL("MortonCodes Encode Vec4(uint16, uint16, uint16, uint16)", result, expected);
+  }
+
+  static void Test_MortonCodes_Decode_uint32()
+  {
+    constexpr uint32 v = 0b01'010'101'010'101'010'101'010'101'010'101;
+
+    constexpr uint16 x = 0b1'111'111'111'111'111;
+    constexpr uint16 y = 0b0'000'000'000'000'000;
+
+    constexpr vec2_t<uint16> expected(x, y);
+    constexpr auto result = Decode(v);
+
+    KRYS_EXPECT_EQUAL("MortonCodes Decode uint32", result, expected);
+  }
+
+#pragma endregion uint16
+
+#pragma region int16
+
+  static void Test_MortonCodes_Encode_2_int16()
+  {
+    constexpr int16 x = 0b1'111'111'111'111'111;
+    constexpr int16 y = 0b0'000'000'000'000'000;
+
+    constexpr int32 expected = 0b01'010'101'010'101'010'101'010'101'010'101;
+    constexpr int32 result = Encode(x, y);
+
+    KRYS_EXPECT_EQUAL("MortonCodes Encode (int16, int16)", result, expected);
+  }
+
+  static void Test_MortonCodes_Encode_Vec2_int16()
+  {
+    constexpr int16 x = 0b1'111'111'111'111'111;
+    constexpr int16 y = 0b0'000'000'000'000'000;
+
+    constexpr vec2_t<int16> v(x, y);
+
+    constexpr int32 expected = 0b01'010'101'010'101'010'101'010'101'010'101;
+    constexpr int32 result = Encode(v);
+
+    KRYS_EXPECT_EQUAL("MortonCodes Encode Vec3(int16, int16)", result, expected);
+  }
+
+  static void Test_MortonCodes_Encode_3_int16()
+  {
+    constexpr int16 x = 5;
+    constexpr int16 y = 9;
+    constexpr int16 z = 1;
+
+    constexpr int64 expected = 1'095;
+    constexpr int64 result = Encode(x, y, z);
+
+    KRYS_EXPECT_EQUAL("MortonCodes Encode (int16, int16, int16)", result, expected);
+  }
+
+  static void Test_MortonCodes_Encode_Vec3_int16()
+  {
+    constexpr int16 x = 5;
+    constexpr int16 y = 9;
+    constexpr int16 z = 1;
+    constexpr vec3_t<int16> v(x, y, z);
+
+    constexpr int32 expected = 1'095;
+    constexpr int32 result = Encode(v);
+
+    KRYS_EXPECT_EQUAL("MortonCodes Encode Vec3(int16, int16, int16)", result, expected);
+  }
+
+  static void Test_MortonCodes_Encode_4_int16()
+  {
+    constexpr int16 x = 0b1'111'111'111'111'111;
+    constexpr int16 y = 0b1'111'111'111'111'111;
+    constexpr int16 z = 0b0'000'000'000'000'000;
+    constexpr int16 w = 0b0'000'000'000'000'000;
+
+    constexpr int64 expected =
+      0b0'011'001'100'110'011'001'100'110'011'001'100'110'011'001'100'110'011'001'100'110'011;
+    constexpr int64 result = Encode(x, y, z, w);
+
+    KRYS_EXPECT_EQUAL("MortonCodes Encode (int16, int16, int16, int16)", result, expected);
+  }
+
+  static void Test_MortonCodes_Encode_Vec4_int16()
+  {
+    constexpr int16 x = 0b1'111'111'111'111'111;
+    constexpr int16 y = 0b1'111'111'111'111'111;
+    constexpr int16 z = 0b0'000'000'000'000'000;
+    constexpr int16 w = 0b0'000'000'000'000'000;
+    constexpr vec4_t<int16> v(x, y, z, w);
+
+    constexpr int64 expected =
+      0b0'011'001'100'110'011'001'100'110'011'001'100'110'011'001'100'110'011'001'100'110'011;
+    constexpr int64 result = Encode(v);
+
+    KRYS_EXPECT_EQUAL("MortonCodes Encode Vec4(int16, int16, int16, int16)", result, expected);
+  }
+
+#pragma endregion int16
+
+#pragma region uint32
 
   static void Test_MortonCodes_Encode_2_uint32()
   {
@@ -90,6 +367,19 @@ namespace Krys::Tests
     KRYS_EXPECT_EQUAL("MortonCodes Encode (uint32, uint32)", result, expected);
   }
 
+  static void Test_MortonCodes_Encode_Vec2_uint32()
+  {
+    constexpr uint32 x = 0b11'111'111'111'111'111'111'111'111'111'111;
+    constexpr uint32 y = 0b00'000'000'000'000'000'000'000'000'000'000;
+    constexpr vec2_t<uint32> v(x, y);
+
+    constexpr uint64 expected =
+      0b0'101'010'101'010'101'010'101'010'101'010'101'010'101'010'101'010'101'010'101'010'101;
+    constexpr uint64 result = Encode(v);
+
+    KRYS_EXPECT_EQUAL("MortonCodes Encode Vec2(uint32, uint32)", result, expected);
+  }
+
   static void Test_MortonCodes_Encode_3_uint32()
   {
     constexpr uint32 x = 5;
@@ -101,4 +391,87 @@ namespace Krys::Tests
 
     KRYS_EXPECT_EQUAL("MortonCodes Encode (uint32, uint32, uint32)", result, expected);
   }
+
+  static void Test_MortonCodes_Encode_Vec3_uint32()
+  {
+    constexpr uint32 x = 5;
+    constexpr uint32 y = 9;
+    constexpr uint32 z = 1;
+    constexpr vec3_t<uint32> v(x, y, z);
+
+    constexpr uint64 expected = 1'095;
+    constexpr uint64 result = Encode(v);
+
+    KRYS_EXPECT_EQUAL("MortonCodes Encode Vec3(uint32, uint32, uint32)", result, expected);
+  }
+
+  static void Test_MortonCodes_Decode_uint64()
+  {
+    constexpr uint64 v =
+      0b0'101'010'101'010'101'010'101'010'101'010'101'010'101'010'101'010'101'010'101'010'101;
+
+    constexpr uint32 x = 0b11'111'111'111'111'111'111'111'111'111'111;
+    constexpr uint32 y = 0b00'000'000'000'000'000'000'000'000'000'000;
+    constexpr vec2_t<uint32> expected(x, y);
+
+    constexpr auto result = Decode(v);
+
+    KRYS_EXPECT_EQUAL("MortonCodes Decode uint64", result, expected);
+  }
+
+#pragma endregion uint32
+
+#pragma region int32
+
+  static void Test_MortonCodes_Encode_2_int32()
+  {
+    constexpr int32 x = 0b11'111'111'111'111'111'111'111'111'111'111;
+    constexpr int32 y = 0b00'000'000'000'000'000'000'000'000'000'000;
+
+    constexpr int64 expected =
+      0b0'101'010'101'010'101'010'101'010'101'010'101'010'101'010'101'010'101'010'101'010'101;
+    constexpr int64 result = Encode(x, y);
+
+    KRYS_EXPECT_EQUAL("MortonCodes Encode (int32, int32)", result, expected);
+  }
+
+  static void Test_MortonCodes_Encode_Vec2_int32()
+  {
+    constexpr int32 x = 0b11'111'111'111'111'111'111'111'111'111'111;
+    constexpr int32 y = 0b00'000'000'000'000'000'000'000'000'000'000;
+    constexpr vec2_t<int32> v(x, y);
+
+    constexpr int64 expected =
+      0b0'101'010'101'010'101'010'101'010'101'010'101'010'101'010'101'010'101'010'101'010'101;
+    constexpr int64 result = Encode(v);
+
+    KRYS_EXPECT_EQUAL("MortonCodes Encode Vec2(int32, int32)", result, expected);
+  }
+
+  static void Test_MortonCodes_Encode_3_int32()
+  {
+    constexpr int32 x = 5;
+    constexpr int32 y = 9;
+    constexpr int32 z = 1;
+
+    constexpr int64 expected = 1'095;
+    constexpr int64 result = Encode(x, y, z);
+
+    KRYS_EXPECT_EQUAL("MortonCodes Encode (int32, int32, int32)", result, expected);
+  }
+
+  static void Test_MortonCodes_Encode_Vec3_int32()
+  {
+    constexpr int32 x = 5;
+    constexpr int32 y = 9;
+    constexpr int32 z = 1;
+    constexpr vec3_t<int32> v(x, y, z);
+
+    constexpr int64 expected = 1'095;
+    constexpr int64 result = Encode(v);
+
+    KRYS_EXPECT_EQUAL("MortonCodes Encode Vec3(int32, int32, int32)", result, expected);
+  }
+
+#pragma endregion int32
 }
