ad20532a6e03afa98046e0817dee52f4dab86fda
dylanclarke890 <dylanclarke890@gmail.com>
2025-01-16 21:57:36 +0000

    various colors

--- include/Graphics/Colors.hpp
+++ include/Graphics/Colors.hpp
@@ -12,4 +12,18 @@ namespace Krys::Colors
   static constexpr Vec4 Gray50 = Vec4(0.5f, 0.5f, 0.5f, 1.0f);
   static constexpr Vec4 Coral = Vec4(1.0f, 0.5f, 0.31f, 1.0f);
   static constexpr Vec4 Yellow = Vec4(1.0f, 1.0f, 0.0f, 1.0f);
+  static constexpr Vec4 Orange = Vec4(1.0f, 0.647f, 0.0f, 1.0f);
+  static constexpr Vec4 Purple = Vec4(0.5f, 0.0f, 0.5f, 1.0f);
+  static constexpr Vec4 Pink = Vec4(1.0f, 0.75f, 0.8f, 1.0f);
+  static constexpr Vec4 Brown = Vec4(0.647f, 0.164f, 0.164f, 1.0f);
+  static constexpr Vec4 Cyan = Vec4(0.0f, 1.0f, 1.0f, 1.0f);
+  static constexpr Vec4 Magenta = Vec4(1.0f, 0.0f, 1.0f, 1.0f);
+  static constexpr Vec4 Lime = Vec4(0.0f, 1.0f, 0.0f, 1.0f);
+  static constexpr Vec4 Teal = Vec4(0.0f, 0.5f, 0.5f, 1.0f);
+  static constexpr Vec4 Indigo = Vec4(0.29f, 0.0f, 0.51f, 1.0f);
+  static constexpr Vec4 Maroon = Vec4(0.5f, 0.0f, 0.0f, 1.0f);
+  static constexpr Vec4 Olive = Vec4(0.5f, 0.5f, 0.0f, 1.0f);
+  static constexpr Vec4 Navy = Vec4(0.0f, 0.0f, 0.5f, 1.0f);
+  static constexpr Vec4 Beige = Vec4(0.96f, 0.96f, 0.86f, 1.0f);
+  static constexpr Vec4 Ivory = Vec4(1.0f, 1.0f, 0.94f, 1.0f);
 }
