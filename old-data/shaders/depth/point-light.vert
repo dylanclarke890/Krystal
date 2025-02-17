56e782b050abcbd4c0b67397f773367f3d3b09cd
dylanclarke890 <dylanclarke890@gmail.com>
2025-02-17 23:56:41 +0000

    removed old-data and old-src directories

--- old-data/shaders/depth/point-light.vert
+++
@@ -1,7 +0,0 @@
-#version 450 core
-layout (location = 0) in vec4 i_Position;
-
-void main()
-{
-  gl_Position = i_Position;
-}
--- old-src/Graphics/Shaders/ShaderPreprocessor.cpp
+++
@@ -1,6 +0,0 @@
-#include "Graphics/Shaders/ShaderPreprocessor.h"
-
-namespace Krys::Gfx
-{
-  Nullable<Map<string, string>> ShaderPreprocessor::TemplateKeys;
-}
